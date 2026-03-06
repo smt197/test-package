e50d0987059292dbcd65f49ff9186349e0f0c1fd
smt197 <serignembayet@gmail.com>
2026-03-06 14:53:42 +0000

    add provider sse

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Laravel\Mcp\Server\McpServiceProvider::class, // Ajoutez cette ligne
+    Laravel\Boost\BoostServiceProvider::class, // Si nécessaire
 ];
