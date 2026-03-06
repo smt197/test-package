58f6d2f0cc55aca785f9a9f71606a1cd0e3e8707
smt197 <serignembayet@gmail.com>
2026-03-06 11:47:09 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(Laravel\Mcp\Server\McpServiceProvider::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
